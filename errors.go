90dce8c63c84096a81a1ab592ddf57032da07643
tiendc <tiendc@gmail.com>
2024-09-24 01:01:27 +0700

    Add safe casting functions

--- errors.go
+++ errors.go
@@ -5,6 +5,7 @@ import "errors"
 var (
 	ErrEmpty           = errors.New("container is empty")
 	ErrIndexOutOfRange = errors.New("index out of range")
+	ErrOverflow        = errors.New("overflow")
 	ErrPanic           = errors.New("panic occurred")
 )
 
